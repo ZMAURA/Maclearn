e13a6e5bd5909a6e595b5b07352229c248291e2d
aura <aura@Mac-5730.lan>
2025-05-22 23:42:44 -0400

    fixed daysinMonth const showing the previous month, added div into html for preperation of calendar

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 const currentDay = currentDate.getDate();
 const currentMonth = currentDate.getMonth();
-const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+//adding + 1 because the months index by 0 so we add to make sure its the next.
 
 let months = ["January", "February", "March", "April", "May","June","July","August","September","October","November", "December"];
 
